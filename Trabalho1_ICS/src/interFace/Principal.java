e5bef0e096852e87e95c61f1d26633e96a1f13da
rodrigofegui <rodrigofegui@gmail.com>
2016-04-13 11:44:56 -0300

    Update Principal.java

--- Trabalho1_ICS/src/interFace/Principal.java
+++ Trabalho1_ICS/src/interFace/Principal.java
@@ -5,16 +5,24 @@ package interFace;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
-public class Principal {
-	JFrame janelaPrincipal;
+public class Principal implements Runnable{
+	static JFrame janelaPrincipal = null;
 	BarraDeMenu barraDeMenu = new BarraDeMenu();
 	AreaGrafica areaGrafica = new AreaGrafica();
+	static int inicioBarra = 0;
+	static int inicioSequenciador = 0;
+	static boolean tocando = false;
+	final static boolean executar = true;
 
 	public static void main(String[] args){
-        new Principal ();
+		Principal principal = new Principal();
+        Thread     thread  = new Thread(principal);
+        thread.start();
+        System.gc();
     }
 	
 	public Principal(){
+		System.gc();
 		/*	Criação da nova interface, com um título */
 		janelaPrincipal = new JFrame ("Tocador MIDI - H&R");
 
@@ -23,9 +31,37 @@ public class Principal {
 		janelaPrincipal.setContentPane (areaGrafica.criarPlanoDeConteudo());
 
 		/*	Configurando o tamanho mínimo da janela */
-		janelaPrincipal.setMinimumSize (new Dimension(550, 190));
-		janelaPrincipal.pack ();
+		janelaPrincipal.setSize(new Dimension(550, 190));
+		/*	Desabilitando o redimensionamento da janela */
+		janelaPrincipal.setResizable(false);
+		//janelaPrincipal.pack ();
 		/* Exibição a interface */
 		janelaPrincipal.setVisible (true);
+		janelaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		System.gc();
+	}
+
+	public void run() {
+		while(executar){
+			if (tocando){
+					AreaGrafica.barraDeProgresso.setValue(inicioBarra);
+					retardo(1000);
+					inicioBarra++;
+			}else{
+				try{ 
+					retardo(1000);                                          
+				}catch(Exception e) {
+					System.out.println(e.getMessage());  
+                }
+			}    
+		}
+	}
+	
+	static void retardo (int miliseg)
+	{  
+            try{
+            	Thread.sleep(miliseg);
+            }catch (InterruptedException e) { }
 	}
 }
